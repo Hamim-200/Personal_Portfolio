d70fbcc4271b49ef4992409a5ce573c695cd8b5d
Hamim Ahmed <hamim.ahmed484@gmail.com>
2022-12-13 22:58:37 +0600

    portfolio added

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,9 +1,80 @@
 import React from 'react';
+import './portfolio.css'
+import IMG1 from '../../images/portfolio1.jpg'
+import IMG2 from '../../images/portfolio2.jpg'
+import IMG3 from '../../images/portfolio3.jpg'
+import IMG4 from '../../images/portfolio4.jpg'
+import IMG5 from '../../images/portfolio5.png'
+import IMG6 from '../../images/portfolio6.jpg'
 
 const Portfolio = () => {
     return (
         <section id='portfolio'>
-            <h4>This is portfolio</h4>
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+
+            <div className="container portfolio__container">
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src={IMG1} alt="" srcset="" />
+                    </div>
+                    <h3>This is a portfolio item Title</h3>
+                    <div className='portfolio__item-cta'>
+                        <a href="https://github.com/" className='btn'>Github</a>
+                        <a href="https://hamim-200.github.io/To_Do_List/" className='btn btn-primary'>Show Demo</a>
+                    </div>
+                </article>
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src={IMG2} alt="" srcset="" />
+                    </div>
+                    <h3>This is a portfolio item Title</h3>
+                    <div className='portfolio__item-cta'>
+                        <a href="https://github.com/" className='btn'>Github</a>
+                        <a href="https://hamim-200.github.io/To_Do_List/" className='btn btn-primary'>Show Demo</a>
+                    </div>
+                </article>
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src={IMG3} alt="" srcset="" />
+                    </div>
+                    <h3>This is a portfolio item Title</h3>
+                    <div className='portfolio__item-cta'>
+                        <a href="https://github.com/" className='btn'>Github</a>
+                        <a href="https://hamim-200.github.io/To_Do_List/" className='btn btn-primary'>Show Demo</a>
+                    </div>
+                </article>
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src={IMG4} alt="" srcset="" />
+                    </div>
+                    <h3>This is a portfolio item Title</h3>
+                    <div className='portfolio__item-cta'>
+                        <a href="https://github.com/" className='btn'>Github</a>
+                        <a href="https://hamim-200.github.io/To_Do_List/" className='btn btn-primary'>Show Demo</a>
+                    </div>
+                </article>
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src={IMG5} alt="" srcset="" />
+                    </div>
+                    <h3>This is a portfolio item Title</h3>
+                    <div className='portfolio__item-cta'>
+                        <a href="https://github.com/" className='btn'>Github</a>
+                        <a href="https://hamim-200.github.io/To_Do_List/" className='btn btn-primary'>Show Demo</a>
+                    </div>
+                </article>
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src={IMG6} alt="" srcset="" />
+                    </div>
+                    <h3>This is a portfolio item Title</h3>
+                    <div className='portfolio__item-cta'>
+                        <a href="https://github.com/" className='btn'>Github</a>
+                        <a href="https://hamim-200.github.io/To_Do_List/" className='btn btn-primary'>Show Demo</a>
+                    </div>
+                </article>
+            </div>
         </section>
     );
 };
--- src/components/services/Service.jsx
+++ src/components/services/Service.jsx
@@ -1,10 +1,128 @@
 import React from 'react';
+import './service.css'
+import { BsCheckLg } from 'react-icons/bs'
 
 const Service = () => {
     return (
         <section id='services'>
-            <h2>Service</h2>
+            <h5>What I Offer</h5>
+            <h2>Services</h2>
+
+            <div className="container services__container">
+
+                <article className="service">
+                    <div className="service__head">
+                        <h3>UI/UX Design</h3>
+                    </div>
+
+                    <ul className="service__list">
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+
+                    </ul>
+                </article>
+                {/* End of UI/UX */}
+
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Web Development</h3>
+                    </div>
+
+                    <ul className="service__list">
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+
+                    </ul>
+                </article>
+
+                <article className="service">
+                    <div className="service__head">
+                        <h3>App Development</h3>
+                    </div>
+
+                    <ul className="service__list">
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg className='service__list-icon'></BsCheckLg>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+
+                    </ul>
+                </article>
+
+            </div>
+
         </section>
+
+
+
     );
 };
 
